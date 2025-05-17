b01c3eb0bb07b3df6cc03fa49dc2d263c0f20e4f
Felipe3a <fpsdev3a@gmail.com>
2025-05-17 19:41:24 -0300

    commit

--- aula05/script.js
+++ aula05/script.js
@@ -4,13 +4,27 @@ confirm("Está gostando do curso?");
 prompt("Qual é o seu nome?");*/
 
 
+var  teste = 
+alert(teste); // null
 
-
-
- var n1 = prompt("Digite o primeiro número: ");
+ /*var n1 = prompt("Digite o primeiro número: ");
  var n2 = prompt("Digite o segundo número: ");
+  
 
  var soma = alert(`Asoma entre ${n1} e ${n2} é: ` + (Number(n1) + Number(n2)));
 
  var nome = prompt("Qual é o seu nome?");
 alertL(`Olá ${nome}, seu nome tem ${nome.length} letras`);
+
+
+
+console.log(`A soma entre ${n1} e ${n2} é:   ${Number(n1) + Number(n2)}`); // imprime no console do navegador*/
+
+
+var produto1 = 5;
+var produto2 = 10;
+
+var total = produto1 + produto2;
+
+alert(`O total é: ${total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`);
+
